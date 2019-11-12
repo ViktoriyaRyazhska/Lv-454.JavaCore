ed07abd5366a674d23c348a5d93eae828075e6dc
Yuriy <55993411+Balu327@users.noreply.github.com>
2019-11-12 16:12:36 +0200

    Add files via upload

---
+++ HW2/Example2.java
@@ -0,0 +1,27 @@
+package edu.com;
+
+import java.io.*;
+
+public class Example {
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("What is your name?");
+        String name = br.readLine();
+
+        System.out.println("Where are you live?");
+        String region = br.readLine();
+
+
+        System.out.println("Your information" + region + "  " + name);
+
+
+
+
+
+
+
+    }
+}
