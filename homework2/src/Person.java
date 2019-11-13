8ead611c79f76974c88b295c9777256053b90c57
Oksana Lytvynchuk <olitvinchuk1212@gmail.com>
2019-11-13 17:20:19 +0200

    Added homework2

---
+++ homework2/src/Person.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Calendar;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public int getBirthYear() {
+        return birthYear;
+    }
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public Person() {}
+
+    public void changeName(String firstName){
+    this.firstName = firstName;
+
+    }
+
+    public  int getAge(){
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        return year - this.birthYear;
+    }
+    public void input() throws IOException {
+        BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+        System.out.println("Enter first name");
+        this.firstName = br.readLine();
+        System.out.println("Enter last name");
+        this.lastName = br.readLine();
+        System.out.println("Enter year your birth");
+        this.birthYear = Integer.parseInt(br.readLine());
+
+    }
+
+    public void print() {
+        System.out.println("Person " + firstName + " " +  lastName + " " + birthYear + " year of birth");
+    }
+    public static void main(String[] args) throws IOException {
+
+        Person person1 = new Person();
+        Person person2 = new Person();
+        Person person3 = new Person();
+        Person person4 = new Person();
+        Person person5 = new Person();
+
+        person1.input();
+        person1.print();
+        person1.getAge();
+        System.out.println("You are " + person1.getAge() + " years old");
+        person1.changeName("Julia");
+        person1.print();
+        person2.getAge();
+        person3.getAge();
+        person4.getAge();
+        person5.getAge();
+    }
+}
