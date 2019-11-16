88f6c62f4df00e1a1dde58dc8f459e1a2970dc36
Oksana Lytvynchuk <olitvinchuk1212@gmail.com>
2019-11-16 17:48:36 +0200

    Added several comments to file Dog

--- Nomework3/src/Dog.java
+++ Nomework3/src/Dog.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Dog {
     private String name;
     private int age;
-    Breed breed;
+    private Breed breed;
 
     public Dog(String name, int age, Breed breed ) {
         this.name = name;
@@ -33,7 +33,6 @@ public class Dog {
     public void setBreed(Breed breed) {
         this.breed = breed;
     }
-
     @Override
     public String toString() {
         return "Dog{" +
@@ -44,8 +43,8 @@ public class Dog {
     }
     public static void main(String[] args) {
         Dog bobtail = new Dog("Lora",1, Breed.BOBTAIL);
-        Dog schnauzer = new Dog("Remy",7, Breed.SCHNAUZER);
-        Dog bulldog = new Dog("Lora",3,Breed.BULLDOG);
+        Dog schnauzer = new Dog("Remmy",7, Breed.SCHNAUZER);
+        Dog bulldog = new Dog("Remmy",10,Breed.BULLDOG);
         Dog[]dogs = {bobtail,schnauzer,bulldog};
         Dog oldestDog = dogs[0];
         for(int i = 1; i< dogs.length; i++){
@@ -54,16 +53,48 @@ public class Dog {
         }
         System.out.println(oldestDog.getBreed()+ " " + oldestDog.getName() + " is the oldest dog");
         boolean isTheSame = false;
-        for(int i = 0; i< dogs.length; i++) {
-            Dog currentDog = dogs[i];
+        /* Який з варіантів оптимальний на вашу думку, щоб визначити чи є пси з однаковими іменами?
+        Варіант перший:
+       for(int i = 0; i< dogs.length; i++) {
+           Dog currentDog = dogs[i];
             for (int j = i + 1; j < dogs.length; j++) {
                 if (currentDog.getName().equals(dogs[j].getName())){
                     isTheSame = true;
                     break;
                 }
+                if(isTheSame == true){
+                    break;
+                }
             }
+        }*/
+
+        /* Варіант  другий:
+        int i = 0;
+        int j = 0;
+        while(i< dogs.length) {
+            Dog currentDog = dogs[i];
+            j = i+1;
+           while(j < dogs.length) {
+                if (currentDog.getName().equals(dogs[j].getName())){
+                    isTheSame = true;
+                    break;
+                } j++;
+            }
+            if(isTheSame == true){
+                break;
+            }
+            i++;
         }
         System.out.println(isTheSame);
+    }      Варіант третій лишаю робочим незакоментованим */
+
+        if (bobtail.getName().equals(schnauzer.getName())||bobtail.getName().equals(bulldog.getName())){
+            System.out.println (isTheSame=true);
+        }else if(schnauzer.getName().equals(bulldog.getName()) ){
+            System.out.println(isTheSame=true);
+        }else{
+            System.out.println(isTheSame=false);
+        }
     }
 }
 
