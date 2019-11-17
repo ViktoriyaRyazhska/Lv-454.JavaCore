fcd862ea778d2fd88ecf288034a135718dac1675
Oksana Lytvynchuk <olitvinchuk1212@gmail.com>
2019-11-17 17:44:01 +0200

    Created Junit tests files for all .java

---
+++ Nomework3/src/test/ErrorTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ErrorTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void main() {
+    }
+}
