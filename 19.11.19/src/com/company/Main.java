5419bc72041580809648f347c3dedcc3c532aa8f
Roman0607 <nashpatsa@gmail.com>
2019-11-19 23:12:25 +0200

    19.10.19

--- 19.11.19/src/com/company/Main.java
+++ 19.11.19/src/com/company/Main.java
@@ -51,7 +51,7 @@ public class Main {
     static int SumPos(int[] arr) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < 0) {
+            if (arr[i] > 0) {
                 sum += arr[i];
             }
         }
