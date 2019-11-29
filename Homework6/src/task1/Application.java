9aabd0df4c97411e9a1526a307460a3a3ec97447
Oksana Lytvynchuk <olitvinchuk1212@gmail.com>
2019-11-29 21:12:41 +0200

    Added folder Homework6

---
+++ Homework6/src/task1/Application.java
@@ -0,0 +1,10 @@
+package task1;
+//Write parameterized methods union(Set set1, Set set2) and intersect(Set set1, Set set2),
+// realizing the operations of union and intersection of two sets.
+// Test the operation of these techniques on two pre-filled sets.
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Homework6/src/task2/Application2.java
@@ -0,0 +1,11 @@
+package task2;
+//Create map personMap and add to it ten persons of type <String, String> (lastName, firstName).
+//Output the entities of the map on the screen.
+//There are at less two persons with the same firstName among these 10 people?
+//Remove from the map person whose firstName is ”Orest” (or other). Print result.
+
+public class Application2 {
+    public static void main(String[] args) {
+
+    }
+}
