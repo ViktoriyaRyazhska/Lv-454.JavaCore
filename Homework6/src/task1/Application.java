79be5e312c6faf19ab8b6ceac2de309d396d8207
Oksana Lytvynchuk <olitvinchuk1212@gmail.com>
2019-12-01 20:07:14 +0200

    Added solution to task1 Homework6

--- Homework6/src/task1/Application.java
+++ Homework6/src/task1/Application.java
@@ -3,8 +3,38 @@ package task1;
 // realizing the operations of union and intersection of two sets.
 // Test the operation of these techniques on two pre-filled sets.
 
-public class Application {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+public class Application {
+
+    public static void main(String[] args) {
+     Set<Integer> set1 = new HashSet<Integer>();
+     Set<Integer> set2 = new HashSet<Integer>();
+     set1.addAll(Arrays.asList(10,12,13,24,35,44,93,45,100,2));
+     set2.addAll(Arrays.asList(24,55,90,99,2,13,25,48,39,41));
+
+     intersectionSets(set1,set2);
+     unionSets(set1,set2);
+     unionTwoSet(set1,set2);
+
+    }
+    public static void unionSets(Set<Integer>hashSet,Set<Integer>set){
+        Set<Integer> union = hashSet;
+        hashSet.addAll(set);
+        System.out.println(union);
+    }
+    public  static void unionTwoSet(Set<Integer>hashSet, Set<Integer>set){
+        Set<Integer> combined = Stream.concat(hashSet.stream(), set.stream())
+                .collect(Collectors.toSet());
+        System.out.println(combined);
+    }
+    public static void intersectionSets(Set<Integer>set1, Set<Integer>set2){
+        Set<Integer> mutalSets = new HashSet<Integer>(set1);
+        mutalSets.retainAll(set2);
+        System.out.println(mutalSets);
     }
 }
