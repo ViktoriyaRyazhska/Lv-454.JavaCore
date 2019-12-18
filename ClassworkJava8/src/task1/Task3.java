d578206247000fe818264c4080e65a9fe8a5814b
Oksana Lytvynchuk <olitvinchuk1212@gmail.com>
2019-12-18 17:34:17 +0200

    add task3 to ClassWork

---
+++ ClassworkJava8/src/task1/Task3.java
@@ -0,0 +1,18 @@
+package task1;
+
+import java.util.Scanner;
+public class Task3 {
+
+    public static void execute(){
+        double amountDollars;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Input cost of USD, ");
+        double costOfUSD=scanner.nextDouble();
+        System.out.println("Input sum of ukrainian gryvnas");
+        double sumOfGryvna=scanner.nextDouble();
+        if (costOfUSD != 0 && sumOfGryvna != 0) {
+            amountDollars = sumOfGryvna / costOfUSD;
+                System.out.println("If you have " + sumOfGryvna + " gryvnas, you can by " + amountDollars + " USD.");
+        } scanner.close();
+    }
+}
